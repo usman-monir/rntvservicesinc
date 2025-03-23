153937684dd243b6918c7fe93c7c3f7c87f82378
Usman Monir <usmanmonir026@gmail.com>
2025-03-23 14:25:57 +0500

    initial commit

---
+++ wp-content/themes/leverage-child/main.js
@@ -0,0 +1,23 @@
+/*----------------------------------------------
+*
+* [Main Scripts]
+*
+* Theme    : Leverage
+* Version  : 1.0.0
+* Author   : Codings
+* Support  : codings.dev
+* 
+----------------------------------------------*/
+
+/*----------------------------------------------
+*
+* If you activated the Child Theme, the scripts placed here will work.
+* 
+----------------------------------------------*/
+
+jQuery(function ($) {
+  'use strict';
+
+  // Your custom script here
+
+})
